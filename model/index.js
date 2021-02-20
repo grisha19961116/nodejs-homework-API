844288f6eab5b37d7f68b7c922ad0b8c05b3729c
unknown <grisha1996111616@gmail.com>
2021-02-20 19:29:15 +0000

    i've done my home work

--- model/index.js
+++ model/index.js
@@ -1,15 +1,90 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+const fs = require("fs/promises");
+const path = require("path");
+const pathContacts = path.join(__dirname, "contacts.json");
 
-const listContacts = async () => {}
+const listContacts = async () => {
+  try {
+    const allContacts = await fs.readFile(pathContacts);
+    const parsedContacts = await JSON.parse(allContacts);
+    return parsedContacts;
+  } catch (e) {
+    return e;
+  }
+};
 
-const getContactById = async (contactId) => {}
+const getContactById = async (contactId) => {
+  try {
+    const allContacts = await fs.readFile(pathContacts);
+    const parsedContacts = await JSON.parse(allContacts);
+    const findContact = await parsedContacts.find((el) => el.id === contactId);
+    if (!findContact) {
+      return null;
+    } else {
+      return findContact;
+    }
+  } catch (e) {
+    return e;
+  }
+};
+const addContact = async (newContact) => {
+  try {
+    const allContacts = await fs.readFile(pathContacts);
+    const parsedContacts = await JSON.parse(allContacts);
+    const addNewContact = [...parsedContacts, newContact];
+    const contactsString = await JSON.stringify(addNewContact, null, 2);
+    await fs.writeFile(pathContacts, contactsString);
+  } catch (e) {
+    return e;
+  }
+};
 
-const removeContact = async (contactId) => {}
+const removeContact = async (contactId) => {
+  try {
+    const allContacts = await fs.readFile(pathContacts);
+    const parsedContacts = await JSON.parse(allContacts);
+    const deletedContact = parsedContacts.filter(
+      (elem) => elem.id !== contactId
+    );
+    if (deletedContact.length === parsedContacts.length) {
+      return false;
+    }
+    const contactsString = await JSON.stringify(deletedContact, null, 2);
+    await fs.writeFile(pathContacts, contactsString);
+    return true;
+  } catch (e) {
+    return e;
+  }
+};
 
-const addContact = async (body) => {}
-
-const updateContact = async (contactId, body) => {}
+const updateContact = async (contactId, body) => {
+  try {
+    const { name, email, phone } = body;
+    const allContacts = await fs.readFile(pathContacts);
+    const parsedContacts = await JSON.parse(allContacts);
+    let findContact;
+    const updatedContacts = await parsedContacts.map((elem) => {
+      if (elem.id === contactId) {
+        findContact = elem;
+        if (name) {
+          findContact.name = name;
+        }
+        if (email) {
+          findContact.email = email;
+        }
+        if (phone) {
+          findContact.phone = phone;
+        }
+        return findContact;
+      }
+      return elem;
+    });
+    const contactsString = await JSON.stringify(updatedContacts, null, 2);
+    await fs.writeFile(pathContacts, contactsString);
+    return findContact;
+  } catch (e) {
+    return e;
+  }
+};
 
 module.exports = {
   listContacts,
@@ -17,4 +92,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
---
+++ routes/api/validationContacts.js
@@ -0,0 +1,32 @@
+const Joi = require("joi");
+
+const schemaCreateContact = Joi.object({
+  name: Joi.string().alphanum().min(3).max(30).required().trim(),
+  email: Joi.string().email().min(3).max(20).required(),
+  phone: Joi.number().required(),
+});
+
+const schemaUpdateContact = Joi.object({
+  name: Joi.string().alphanum().min(3).max(30).optional().trim(),
+  email: Joi.string().email().min(3).max(20).optional(),
+  phone: Joi.number().optional(),
+});
+const validate = (schema, obj, next) => {
+  const { error } = schema.validate(obj);
+  if (error) {
+    const [{ message }] = error.details;
+    return next({
+      status: 400,
+      message: `Filed: ${message.replace(/"/g, "")}`,
+    });
+  }
+  next();
+};
+
+module.exports.createContact = (req, _res, next) => {
+  return validate(schemaCreateContact, req.body, next);
+};
+
+module.exports.updateUpdate = (req, _res, next) => {
+  return validate(schemaUpdateContact, req.body, next);
+};
